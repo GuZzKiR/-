307f10112f5b47cb0c2007a9338c28438c115ca6
Kirill <guzz1988@mail.ru>
2022-08-28 13:37:53 +0300

    hellolike

---
+++ HelloLIkes/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if(username.ToLower()=="кирилл")
+{
+    Console.WriteLine ("Здорово братан!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
